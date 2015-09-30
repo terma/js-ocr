8957a872c60db9286219954f419c4cd2d2712dab
terma <artem.stasuk@gmail.com>
2015-09-30 06:43:20 -0400

    Refactoring

--- demo.js
+++ demo.js
@@ -27,15 +27,18 @@ var App = angular.module('App', []);
 App.controller('controller', ['$scope', function ($scope) {
 
     $scope.fontSize = 24;
-
-    $scope.testText = 'Just take a car.\n' +
-        'Total 10.00\n' +
-        'Mama\n' +
-        'Don\'t cry!';
+    $scope.testText = 'Item        Qty Price Amount\n' +
+        '----------------------------\n' +
+        'Milk       2Gal 15.00  15.00\n' +
+        'Watermelon  3.5  2.00   7.00\n' +
+        '              Subtotal 23.00\n' +
+        '                  Tax:  5.25\n' +
+        '----------------------------\n' +
+        '                Total: 28.25';
 
     $scope.$watch('testText', function () {
         var canvas = document.getElementById('original');
-        canvas.width = canvas.width;
+        canvas.width = canvas.width; // reset canvas
         var ctx = canvas.getContext('2d');
 
         angular.forEach($scope.testText.split('\n'), function (line, lineIndex) {
@@ -45,138 +48,39 @@ App.controller('controller', ['$scope', function ($scope) {
         $scope.recognized();
     });
 
-    function horizontalLineSumColor(image, x, y0, y1) {
-        var sum = 0;
-        for (var y = y0; y < y1; y++) {
-            var redPixel = image.data[(y * image.width * 4) + (x * 4) + 0];
-            var greenPixel = image.data[(y * image.width * 4) + (x * 4) + 1];
-            var bluePixel = image.data[(y * image.width * 4) + (x * 4) + 2];
-            var alphaPixel = image.data[(y * image.width * 4) + (x * 4) + 3];
-            //if (redPixel > 0 || greenPixel > 0 || bluePixel > 0 || alphaPixel > 0)
-            //    console.log('R:' + redPixel + 'G:' + greenPixel + 'B:' + bluePixel + 'A:' + alphaPixel);
-            sum += alphaPixel;
-        }
-        return sum;
-    }
-
-    function recongnizeCharacters(imageData) {
-        var wordCoords = [];
-
-        var linesCoords = recongnizeLines(imageData);
-
-        angular.forEach(linesCoords, function (lineCoord) {
-            var startLineX = void 0;
-            for (var x = 0; x < imageData.width; x++) {
-                var lineSum = horizontalLineSumColor(imageData, x, lineCoord.start, lineCoord.end);
-
-                if (lineSum < 350) {
-                    if (!startLineX) {
-                        // skip just additional white line
-                    } else {
-                        wordCoords.push({
-                            x0: startLineX,
-                            x1: x - 1,
-                            y0: lineCoord.start,
-                            y1: lineCoord.end
-                        });
-                        startLineX = void 0;
-                    }
-                } else {
-                    if (!startLineX) {
-                        startLineX = x;
-                    }
-                }
-            }
-        });
-
-        console.log('word coords:');
-        console.log(wordCoords);
-        return wordCoords;
-    }
-
-    function recongnizeLines(imageData) {
-        function verticalLineSumColor(image, line) {
-            var sum = 0;
-            for (var x = 0; x < image.width; x++) {
-                var redPixel = image.data[(line * image.width * 4) + (x * 4) + 0];
-                var greenPixel = image.data[(line * image.width * 4) + (x * 4) + 1];
-                var bluePixel = image.data[(line * image.width * 4) + (x * 4) + 2];
-                var alphaPixel = image.data[(line * image.width * 4) + (x * 4) + 3];
-                //if (redPixel > 0 || greenPixel > 0 || bluePixel > 0 || alphaPixel > 0)
-                //    console.log('R:' + redPixel + 'G:' + greenPixel + 'B:' + bluePixel + 'A:' + alphaPixel);
-                sum += alphaPixel;
-            }
-            return sum;
-        }
-
-        var startLineY = void 0;
-        var linesCoordinates = [];
-
-        for (var y = 0; y < imageData.height; y++) {
-            var lineSum = verticalLineSumColor(imageData, y);
-
-            if (lineSum === 0) {
-                if (!startLineY) {
-                    // skip just additional white line
-                } else {
-                    linesCoordinates.push({start: startLineY, end: y - 1});
-                    startLineY = void 0;
-                }
-            } else {
-                if (!startLineY) {
-                    startLineY = y;
-                }
-            }
-        }
-
-        return linesCoordinates;
-    }
-
     $scope.recognized = function () {
         var originalCanvas = document.getElementById('original');
         var originalCtx = originalCanvas.getContext('2d');
         var originalImage = originalCtx.getImageData(0, 0, originalCanvas.width, originalCanvas.height);
 
-        var characterCoords = recongnizeCharacters(originalImage);
+        var result = recognize(originalImage, $scope.fontSize);
 
-        try {
-            var characterOcr = new CharacterOcr($scope.fontSize);
-            var studyCases = characterOcr.study();
+        document.getElementById('studyImages').innerHTML = '';
+        result.studyCases.forEach(function (studyCase) {
+            var img = document.createElement('img');
+            img.src = studyCase.url;
+            document.getElementById('studyImages').appendChild(img);
+        });
 
-            document.getElementById('studyImages').innerHTML = '';
-            for (var i = 0; i < studyCases.length; i++) {
-                var img = document.createElement('img');
-                img.src = studyCases[i].url;
-                document.getElementById('studyImages').appendChild(img);
-            }
+        document.getElementById('recognized').innerText = '';
+        var networkResult = '';
+        angular.forEach(result.result.result, function (item) {
+            document.getElementById('recognized').innerText += item.character;
 
-            // reco
-            var recognize = characterOcr.recognize(originalImage, characterCoords);
-
-            document.getElementById('recognized').innerText = '';
-
-            var networkResult = '';
-
-            angular.forEach(recognize.result, function (item) {
-                document.getElementById('recognized').innerText += item.character;
-
-                networkResult += item.character + ' ';
-                angular.forEach(item.neuronOutputs, function (o, index) {
-                    networkResult += characterOcr.characters[index] + '=' + Math.round(o) + ' ';
-                });
-                networkResult += '<p>'
+            networkResult += item.character + ' ';
+            angular.forEach(item.neuronOutputs, function (o, index) {
+                networkResult += result.characterOcr.characters[index] + '=' + Math.round(o) + ' ';
             });
-            document.getElementById('networkResult').innerHTML = networkResult;
+            networkResult += '<p>'
+        });
+        document.getElementById('networkResult').innerHTML = networkResult;
 
-            document.getElementById('characterImages').innerHTML = '';
-            for (var i = 0; i < recognize.characterImages.length; i++) {
-                var img = document.createElement('img');
-                img.src = recognize.characterImages[i];
-                document.getElementById('characterImages').appendChild(img);
-            }
-        } catch (e) {
-            console.log(e);
-        }
+        document.getElementById('characterImages').innerHTML = '';
+        result.result.characterImages.forEach(function (characterImage) {
+            var img = document.createElement('img');
+            img.src = characterImage;
+            document.getElementById('characterImages').appendChild(img);
+        });
 
         // ---
 
@@ -184,7 +88,7 @@ App.controller('controller', ['$scope', function ($scope) {
         resultCanvas.width = resultCanvas.width;
         var resultCtx = resultCanvas.getContext('2d');
 
-        angular.forEach(characterCoords, function (textCoord) {
+        angular.forEach(result.characterCoords, function (textCoord) {
             //ctx.font = 'bold 14px sans-serif';
             resultCtx.strokeStyle = "#00FF00";
 
--- js-ocr.js
+++ js-ocr.js
@@ -252,3 +252,105 @@ function drawCharacter(canvas, character, rotation, fontSize) {
 String.prototype.capitalizeFirstLetter = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
+
+
+function horizontalLineSumColor(image, x, y0, y1) {
+    var sum = 0;
+    for (var y = y0; y < y1; y++) {
+        var redPixel = image.data[(y * image.width * 4) + (x * 4) + 0];
+        var greenPixel = image.data[(y * image.width * 4) + (x * 4) + 1];
+        var bluePixel = image.data[(y * image.width * 4) + (x * 4) + 2];
+        var alphaPixel = image.data[(y * image.width * 4) + (x * 4) + 3];
+        //if (redPixel > 0 || greenPixel > 0 || bluePixel > 0 || alphaPixel > 0)
+        //    console.log('R:' + redPixel + 'G:' + greenPixel + 'B:' + bluePixel + 'A:' + alphaPixel);
+        sum += alphaPixel;
+    }
+    return sum;
+}
+
+function recongnizeCharacters(imageData) {
+    var wordCoords = [];
+
+    var linesCoords = recongnizeLines(imageData);
+
+    angular.forEach(linesCoords, function (lineCoord) {
+        var startLineX = void 0;
+        for (var x = 0; x < imageData.width; x++) {
+            var lineSum = horizontalLineSumColor(imageData, x, lineCoord.start, lineCoord.end);
+
+            if (lineSum < 350) {
+                if (!startLineX) {
+                    // skip just additional white line
+                } else {
+                    wordCoords.push({
+                        x0: startLineX,
+                        x1: x - 1,
+                        y0: lineCoord.start,
+                        y1: lineCoord.end
+                    });
+                    startLineX = void 0;
+                }
+            } else {
+                if (!startLineX) {
+                    startLineX = x;
+                }
+            }
+        }
+    });
+
+    console.log('word coords:');
+    console.log(wordCoords);
+    return wordCoords;
+}
+
+function recongnizeLines(imageData) {
+    function verticalLineSumColor(image, line) {
+        var sum = 0;
+        for (var x = 0; x < image.width; x++) {
+            var redPixel = image.data[(line * image.width * 4) + (x * 4) + 0];
+            var greenPixel = image.data[(line * image.width * 4) + (x * 4) + 1];
+            var bluePixel = image.data[(line * image.width * 4) + (x * 4) + 2];
+            var alphaPixel = image.data[(line * image.width * 4) + (x * 4) + 3];
+            //if (redPixel > 0 || greenPixel > 0 || bluePixel > 0 || alphaPixel > 0)
+            //    console.log('R:' + redPixel + 'G:' + greenPixel + 'B:' + bluePixel + 'A:' + alphaPixel);
+            sum += alphaPixel;
+        }
+        return sum;
+    }
+
+    var startLineY = void 0;
+    var linesCoordinates = [];
+
+    for (var y = 0; y < imageData.height; y++) {
+        var lineSum = verticalLineSumColor(imageData, y);
+
+        if (lineSum === 0) {
+            if (!startLineY) {
+                // skip just additional white line
+            } else {
+                linesCoordinates.push({start: startLineY, end: y - 1});
+                startLineY = void 0;
+            }
+        } else {
+            if (!startLineY) {
+                startLineY = y;
+            }
+        }
+    }
+
+    return linesCoordinates;
+}
+
+function recognize(image, fontSize) {
+    var characterOcr = new CharacterOcr(fontSize);
+    var studyCases = characterOcr.study();
+
+    var characterCoords = recongnizeCharacters(image);
+    var recognize = characterOcr.recognize(image, characterCoords);
+    return {
+        characterOcr: characterOcr,
+        studyCases: studyCases,
+        result: recognize,
+        characterCoords: characterCoords
+    };
+}
